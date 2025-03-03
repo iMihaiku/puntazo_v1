44a6e0e28386f1f90e63c42139a4efb849747954
= <=>
2025-03-03 09:29:53 +0100

    fix build

--- src/app/oauth/page.tsx
+++ src/app/oauth/page.tsx
@@ -4,13 +4,11 @@ import { useSearchParams } from 'next/navigation'
 import { useEffect } from 'react'
 
 export default function Page() {
-  const router = useSearchParams()
-  const status = router.get('state')
   useEffect(() => {
     // Cerrar la pestaña después de un breve retraso para asegurar que el estado se haya leído
     setTimeout(() => {
       window.close()
     }, 1000)
   }, [])
-  return <div>{status}</div>
+  return <div></div>
 }
